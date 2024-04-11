bc4a33f19092b99deb9282af0b6721e171eba6e3
Francisco Manuel Gamero Rodríguez <gamero.rofra24@triana.salesianos.edu>
2024-04-11 14:17:30 +0200

    Estructura general de la calculadora

--- Calculadora/src/ejercicio/Principal.java
+++ Calculadora/src/ejercicio/Principal.java
@@ -1,10 +1,34 @@
 package ejercicio;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a, b, opcion;
+		Scanner sc = new Scanner(System.in);
+		String aux;
+
+		System.out.println("""
+				1.Sumar
+				2.Restar
+				3.Multiplicar
+				4.Dividir
+				""");
+		aux = sc.nextLine();
+		opcion = Integer.parseInt(aux);
+		switch (opcion) {
+		case 1:
+
+		case 2:
+
+		case 3:
+
+		case 4:
+
+		default:
+		}
 
 	}
-
 }
